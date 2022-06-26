c141c592a2e3906352a63fcb94ab60b15ede094b
Thapelo Chimfwembe <78848435+OluwaMukulu@users.noreply.github.com>
2022-06-26 03:36:41 +0200

    adding search and filter to all admin views

--- cccdata/admin.py
+++ cccdata/admin.py
@@ -13,37 +13,53 @@ class EmployeeAdmin(admin.ModelAdmin):
 
 class MenuAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'type', 'country_of_origin')
+    search_fields = ['name']
+    list_filter = ('type', 'country_of_origin')
 
 class PartnerAdmin(admin.ModelAdmin):
     list_display = ('name', 'phone', 'email', 'address')
+    search_fields = ['name']
 
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'menu','unit_serving')
+    search_fields = ['name']
 
 class SupplierAdmin(admin.ModelAdmin):
     list_display = ('name', 'phone', 'email', 'address')
+    search_fields = ['name']
 
 class ClientAdmin(admin.ModelAdmin):
     list_display = ('name', 'phone', 'email', 'address')
+    search_fields = ['name']
 
 class IngredientsAdmin(admin.ModelAdmin):
     list_display = ('name','item', 'supplier', 'unit_price','quantity', 'profit_per_serving')
+    search_fields = ['name']
 
 class InventoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'type', 'supplier', 'unit_price', 'quantity', 'date_proccured')
+    search_fields = ['name']
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ('name', 'client', 'event_type','start_date', 'end_date', 'contract_sum', 'ordinary_guests','vip_guests', 'city')
+    search_fields = ['name']
+    list_filter = ('country')
 
 class ActivityAdmin(admin.ModelAdmin):
     list_display = ('name', 'department', 'event','supervisor', 'status')
+    search_fields = ['name']
+    list_filter = ('status', 'supervisor')
 
 class ExpenseAdmin(admin.ModelAdmin):
     list_display = ('name', 'expense_type','activity','event','date', 'unit_price', 'quantity')
+    search_fields = ['name']
+    list_filter = ('expense_type')
 
 
 class PeformanceAdmin(admin.ModelAdmin):
     list_display = ('employee', 'department', 'event', 'salary', 'peformance','date')
+    search_fields = ['employee']
+    list_filter = ('department', 'peformance','event')
 
 admin.site.register(Menu, MenuAdmin)
 admin.site.register(Partner, PartnerAdmin)
