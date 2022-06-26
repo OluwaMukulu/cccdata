2454b790b94fe1ed07633fd3c25cd185a65f7e76
Thapelo Chimfwembe <78848435+OluwaMukulu@users.noreply.github.com>
2022-06-26 03:06:03 +0200

    altered unit price feild to decimal

--- ccc/settings.py
+++ ccc/settings.py
@@ -128,3 +128,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
+
+USE_L10N = True
+USE_THOUSAND_SEPARATOR = True
